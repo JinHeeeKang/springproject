c58182095c51103d0a49303e86466b31a17bf541
강진희 <kangjinhee@ktis.co.kr>
2022-06-29 10:44:17 +0900

    제9절 HTTP 파라미터 처리

--- dynamic-web-app/src/main/java/com/ktis/hello/controller/BbsController.java
+++ dynamic-web-app/src/main/java/com/ktis/hello/controller/BbsController.java
@@ -4,9 +4,7 @@ import com.ktis.hello.dao.vo.Article;
 import com.ktis.hello.service.BbsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/bbs")
@@ -15,14 +13,20 @@ public class BbsController {
     @Autowired
     private BbsService bbsService;
 
-    @RequestMapping(value = "/write",method = RequestMethod.POST)
+    @GetMapping("/{articleId}")
+    public String viewDetail(@PathVariable String articleId){
+        System.out.println("글번호는: "+ articleId);
+        return "write_ok";
+    }
+
+    @PostMapping("/write")
     public String doWrite(){
         System.out.println("POST request");
         bbsService.registArticle(new Article());
         return "write_ok";
     }
 
-    @RequestMapping("/write")
+    @GetMapping("/write")
     public String write(){
         System.out.println("GET request");
         bbsService.registArticle(new Article());
